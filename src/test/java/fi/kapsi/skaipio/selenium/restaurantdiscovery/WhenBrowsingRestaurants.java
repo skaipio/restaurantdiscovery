8530601109507ddc565375abb86595d403c2a717
Senni Kaipio <senni.kaipio@helsinki.fi>
2014-11-22 21:30:06 +0200

    Add an acceptance test for browsing restaurants

---
+++ src/test/java/fi/kapsi/skaipio/selenium/restaurantdiscovery/WhenBrowsingRestaurants.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.kapsi.skaipio.selenium.restaurantdiscovery;
+
+import org.junit.After;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+/**
+ *
+ * @author skaipio
+ */
+public class WhenBrowsingRestaurants {
+    private final String baseUrl = "http://localhost:8080/RestaurantDiscovery";
+    private WebDriver driver;
+    
+    @Before
+    public void openBrowser(){
+        driver = new HtmlUnitDriver();
+    }
+    
+    @After
+    public void closerBrowser(){
+        driver.close();
+    }
+    
+    @Test
+    public void restaurantsAreListedOnMainPage(){
+        driver.get(baseUrl);
+        String pageSource = driver.getPageSource();
+        assertTrue(pageSource.contains("Lappi Restaurant"));
+        assertTrue(pageSource.contains("Classic American Diner"));
+    }
+}
